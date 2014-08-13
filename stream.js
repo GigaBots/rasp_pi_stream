54bbe0c5c987c2a992236df7d16a4db4d06d5d39
John DiBaggio <jsantoku@gmail.com>
2014-08-13 14:50:09 -0500

    stream.js
    stream.js require

--- stream.js
+++ stream.js
@@ -4,28 +4,38 @@
 
 var BrowserBigBangClient = require("./BigBangClient.js");
 var BigBangClient = require("./BigBangClient.js");
-var NodeBigBangClient = require("./NodeBigBangClient.js")
 var PewRuntime = require("./BigBangClient.js");
+var NodeBigBangClient = require("./NodeBigBangClient.js");
 
-var client = new bigbang.client.BigBangClient();
+var websocket = require('websocket');
 
-client.connectAnonymous("thegigabots.app.bigbang.io:80", function(result) {
-	if( result.success) {
-		client.subscribe("newBot", function( err, c) {
-			if(!err) {
-				beginStream(client,c);
-			}
-			else {
-				console.log("Subscribe failure. " + err);
-			}
-		})
-	}
-	else {
-		console.log("Connect Failure.");
-	}
-});
 
-function beginStream(client, channel) {
-	console.log("Begin Stream!!");
+require(['BigBangClient', 'PewRuntime'], function (bigbang, pew) {
+
+	var client = new bigbang.client.BigBangClient();
+
+	client.connectAnonymous("thegigabots.app.bigbang.io:80", function(result) {
+		if( result.success) {
+			client.subscribe("newBot", function( err, c) {
+				if(!err) {
+					beginStream(client,c);
+				}
+				else {
+					console.log("Subscribe failure. " + err);
+				}
+			})
+		}
+		else {
+			console.log("Connect Failure.");
+		}
+	});
+
+	function beginStream(client, channel) {
+		console.log("Begin Stream!!");
+
+		//TODO
+
 }
+
+});
 

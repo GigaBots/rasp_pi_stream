f1a0d1260a74420d6b60fecfde18aa10a4bf33e2
John DiBaggio <jsantoku@gmail.com>
2014-08-13 14:32:01 -0500

    installed requirejs
    installed requirejs

--- stream.js
+++ stream.js
@@ -2,30 +2,46 @@
 * Live video-stream program for Rasp Pi
 */
 
-var BrowserBigBangClient = require("./dist/node/BigBangClient.js");
-var BigBangClient = require("./dist/node/BigBangClient.js");
-var NodeBigBangClient = require("./dist/node/NodeBigBangClient.js")
-var PewRuntime = require("./dist/node/BigBangClient.js");
+var client;
 
-var client = new bigbang.client.BigBangClient();
+var requirejs = require('requirejs');
 
-client.connectAnonymous("thegigabots.app.bigbang.io:80", function(result) {
-	if( result.success) {
-		client.subscribe("newBot", function( err, c) {
-			if(!err) {
-				beginStream(client,c);
-			}
-			else {
-				console.log("Subscribe failure. " + err);
-			}
-		})
-	}
-	else {
-		console.log("Connect Failure.");
-	}
+requirejs.config({
+	baseUrl: '/rasp_pi_stream',
+	nodeRequire: require,
+    // paths: {
+    //     "BrowserBigBangClient": "./dist/node/BigBangClient.js",
+    //     "BigBangClient": "./dist/node/BigBangClient.js",
+    //     "PewRuntime": "./dist/node/BigBangClient.js"
+    // }
 });
 
-function beginStream(client, channel) {
-	console.log("Begin Stream!!");
-}
+var BrowserBigBangClient = requirejs("./BigBangClient.js");
+var PewRuntime = requirejs("./BigBangClient.js");
+
+requirejs(['BrowserBigBangClient', 'PewRuntime'], function (bigbang, pew) {
+
+	client = new bigbang.client.BigBangClient();
+
+	client.connectAnonymous("thegigabots.app.bigbang.io:80", function(result) {
+		if( result.success) {
+			client.subscribe("newBot", function( err, c) {
+				if(!err) {
+					beginStream(client,c);
+				}
+				else {
+					console.log("Subscribe failure. " + err);
+				}
+			})
+		}
+		else {
+			console.log("Connect Failure.");
+		}
+	});
+
+	function beginStream(client, channel) {
+		console.log("Begin Stream!!");
+	}
+
+});
 

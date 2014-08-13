1288e1a9e4b5d006ffe5363f10b1a1cf11295f05
John DiBaggio <jsantoku@gmail.com>
2014-08-13 14:32:24 -0500

    Revert "installed requirejs"
    This reverts commit f1a0d1260a74420d6b60fecfde18aa10a4bf33e2.

--- stream.js
+++ stream.js
@@ -2,46 +2,30 @@
 * Live video-stream program for Rasp Pi
 */
 
-var client;
+var BrowserBigBangClient = require("./dist/node/BigBangClient.js");
+var BigBangClient = require("./dist/node/BigBangClient.js");
+var NodeBigBangClient = require("./dist/node/NodeBigBangClient.js")
+var PewRuntime = require("./dist/node/BigBangClient.js");
 
-var requirejs = require('requirejs');
+var client = new bigbang.client.BigBangClient();
 
-requirejs.config({
-	baseUrl: '/rasp_pi_stream',
-	nodeRequire: require,
-    // paths: {
-    //     "BrowserBigBangClient": "./dist/node/BigBangClient.js",
-    //     "BigBangClient": "./dist/node/BigBangClient.js",
-    //     "PewRuntime": "./dist/node/BigBangClient.js"
-    // }
-});
-
-var BrowserBigBangClient = requirejs("./BigBangClient.js");
-var PewRuntime = requirejs("./BigBangClient.js");
-
-requirejs(['BrowserBigBangClient', 'PewRuntime'], function (bigbang, pew) {
-
-	client = new bigbang.client.BigBangClient();
-
-	client.connectAnonymous("thegigabots.app.bigbang.io:80", function(result) {
-		if( result.success) {
-			client.subscribe("newBot", function( err, c) {
-				if(!err) {
-					beginStream(client,c);
-				}
-				else {
-					console.log("Subscribe failure. " + err);
-				}
-			})
-		}
-		else {
-			console.log("Connect Failure.");
-		}
-	});
-
-	function beginStream(client, channel) {
-		console.log("Begin Stream!!");
+client.connectAnonymous("thegigabots.app.bigbang.io:80", function(result) {
+	if( result.success) {
+		client.subscribe("newBot", function( err, c) {
+			if(!err) {
+				beginStream(client,c);
+			}
+			else {
+				console.log("Subscribe failure. " + err);
+			}
+		})
 	}
-
+	else {
+		console.log("Connect Failure.");
+	}
 });
+
+function beginStream(client, channel) {
+	console.log("Begin Stream!!");
+}
 
